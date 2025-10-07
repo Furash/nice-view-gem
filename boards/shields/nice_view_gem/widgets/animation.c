7b96d40e1fa50121a698868ac2f8a2a6cfb89603
Cyrill Vitkovskiy <c.vitkovskiy@gmail.com>
2025-10-07 09:42:49 +0100

    invert + offset 16 px

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -5,5 +5,5 @@ LV_IMG_DECLARE(frontier_01);
 void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_img_create(canvas);
     lv_img_set_src(art, &frontier_01);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 16);
 }
