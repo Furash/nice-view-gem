63b16574d759f04d7bc76a286a8a5b49d61714f2
Cyrill Vitkovskiy <c.vitkovskiy@gmail.com>
2025-12-19 14:34:14 +0000

    copying over anoither repo

--- boards/shields/nice_view_gem/widgets/animation.c
+++
@@ -1,9 +0,0 @@
-#include <lvgl.h>
-
-LV_IMG_DECLARE(frontier_01);
-
-void draw_animation(lv_obj_t *canvas) {
-    lv_obj_t *art = lv_image_create(canvas);
-    lv_image_set_src(art, &frontier_01);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 20, 0);
-}
