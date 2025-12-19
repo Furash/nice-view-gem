8dbfb40e0b0f56e977f976b6afb45f3073362549
Cyrill Vitkovskiy <c.vitkovskiy@gmail.com>
2025-12-19 14:40:14 +0000

    Revert "copying over anoither repo"
    This reverts commit 63b16574d759f04d7bc76a286a8a5b49d61714f2.

--- ZMK_RAW_HID/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf qmk-hid-host
-rm -rf "ZMK HID Runner.app"
-rm -rf zmk-hid-host
-rm -rf ZMK_HID_Runner_Installer.dmg
---
+++ boards/shields/nice_view_gem/widgets/screen.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <lvgl.h>
+#include <zephyr/kernel.h>
+#include "util.h"
+
+struct zmk_widget_screen {
+    sys_snode_t node;
+    lv_obj_t *obj;
+    lv_color_t cbuf[1024];
+    lv_color_t cbuf2[1024];
+    lv_color_t cbuf3[1024];
+    struct status_state state;
+};
+
+int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_screen_obj(struct zmk_widget_screen *widget);
